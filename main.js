9dff60ea0c97180daff8d37a717cefa7701c2f51
Cody B <67527461+cobull@users.noreply.github.com>
2024-12-12 13:25:43 -0600

    Add comment

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const client = new MongoClient(url);
 // Database Name
 const dbName = 'myDatabase';
 
+// Main loop
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
